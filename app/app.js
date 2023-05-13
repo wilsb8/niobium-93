a67372a0f0f422c6c99d880fe0519e1d442612a8
wilsb8 <wilsb8@gmail.com>
2023-05-13 12:50:19 -0700

    it works

--- app/app.js
+++ app/app.js
@@ -6,8 +6,8 @@ app.set('view engine', 'ejs');
 app.engine('ejs', require('ejs').__express);
 
 //Static site middleware
-app.use(express.static('/public'));
-app.use(express.static('/views'));
+app.use(express.static('public'));
+app.use(express.static('views'));
 
 const router = require('../routes/router')
 app.use('/', router);
